70459a57fb21bda2d290f55328e6574f4d87e80a
Vinicius Silva Queiroz <81514002+ViniciusQueiroz327@users.noreply.github.com>
2026-09-01 19:39:21 -0300

    Adicionando função de ScrollToTop

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 import Emaus from "../pages/Emaus/Emaus";
 import Kadosh from "../pages/Kadosh/Kadosh";
 
 function Router() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Emaus />} />
         <Route path="/emaus" element={<Emaus />} />
